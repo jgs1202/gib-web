0d5979b7ea8c4a6ef28b885d7563600e3bf03a95
Nozomi Aoyama <jgs.kuee@gmail.com>
2018-08-31 02:24:17 +0900

    update

--- templates/src/components/upload.vue
+++ templates/src/components/upload.vue
@@ -138,8 +138,8 @@ export default {
             console.log(this.response)
             let json = JSON.stringify(this.response)
             console.log(json)
-            let blob = new Blob([this.response], {type: 'application/json'})
-            let url = URL.createObjectURL(blob)
+            let blob = new Blob([json], {type: 'application/json'})
+            let url = window.createObjectURL(blob)
             let a = document.createElement('a')
             a.download = 'sample_data.json'
             a.href = url
