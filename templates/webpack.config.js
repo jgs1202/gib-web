30722628b8b4f85797f234dc029e343347a55458
Nozomi Aoyama <jgs.kuee@gmail.com>
2018-08-30 19:59:29 +0900

    update

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       {test: /(\.css$)/, loaders: ['style-loader', 'css-loader']},
       {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'file-loader'},
       {test: /\.js$/, loader: 'babel-loader', include: path.resolve(__dirname, 'node_modules', 'd3-downloadable')},
-      // {test: /\.js$/, loader: 'babel-loader', include: path.resolve(__dirname, 'node_modules', 'svg-dataurl')}
+      {test: /\.js$/, loader: 'babel-loader', include: path.resolve(__dirname, 'node_modules', 'svg-dataurl')}
     ]
   },
   resolve: {
