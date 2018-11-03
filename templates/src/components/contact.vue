3ec8b4cbc2e7ce97d87ac1a03f9e2316f39e553d
Nozomi Aoyama <jgs.kuee@gmail.com>
2018-11-03 02:14:31 +0100

    update

--- templates/src/components/contact.vue
+++ templates/src/components/contact.vue
@@ -30,57 +30,13 @@ export default {
     click: function () {
     },
     download: function(e){
-      // var xmlHttpRequest = new XMLHttpRequest();
-      // let json = null
-      // xmlHttpRequest.onreadystatechange = function() {
-      //   console.log(this.status)
-      //   console.log(this.response)
-      //   if( this.readyState == 4 && this.status == 200 ) {
-      //     if( this.response ) {
-      //       console.log(this.response)
-      //       json = JSON.stringify(this.response, null, '\t')
-      //       console.log(json)
-      //       let blob = new Blob([json], {type: 'application/json'})
-      //       let url = window.URL.createObjectURL(blob)
-      //       let a = document.createElement('a')
-      //       // a.target = '_blank'
-      //       a.download = 'sample_data.json'
-      //       // a.textContent = 'download sample_data.json'
-      //       if (window.navigator.msSaveBlob) {
-      //         // for IE
-      //         window.navigator.msSaveBlob(blob, name)
-      //       }
-      //       else if (window.URL && window.URL.createObjectURL) {
-      //         // for Firefox
-      //         a.href = window.URL.createObjectURL(blob);
-      //         document.body.appendChild(a);
-      //         a.click();
-      //         document.body.removeChild(a);
-      //       }
-      //       else if (window.webkitURL && window.webkitURL.createObject) {
-      //         // for Chrome 
-      //         a.href = window.webkitURL.createObjectURL(blob);
-      //         a.click();
-      //       }
-      //     }
-      //   }
-      // }
-      // xmlHttpRequest.open('GET', 'http://35.233.171.147/image/sample_data.json', true);
-      // // xmlHttpRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
-      // // xmlHttpRequest.setRequestHeader('Content-type', 'application/json');
-      // // xmlHttpRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
-      // xmlHttpRequest.responseType = 'json';
-      // xmlHttpRequest.send( null );
-
+      let json = null
       d3.json('../image/sample_data.json').then(function(graph){
-        console.log(graph)
+        json = graph
+        var obj = json //{a: 123, b: "4 5 6"};
+        var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+        $('<a href="data:' + data + '" download="data.json">download JSON</a>').appendTo('#container')
       })
-
-
-      // var obj = json //{a: 123, b: "4 5 6"};
-      // var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
-
-      // $('<a href="data:' + data + '" download="data.json">download JSON</a>').appendTo('#container')
     }
   }
 }
